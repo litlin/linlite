0bc3150e7c2151d2e02095a44f91796d0894c1ee
linlite <cxiaolinj@163.com>
2016-09-14 11:06:17 +0800

    go on

--- Core/Controller/TestController.php
+++ Core/Controller/TestController.php
@@ -4,13 +4,17 @@ namespace linlite\Core\Controller;
 
 class TestController {
 	public static function run($str1 = "") {
-		$str2 = file_get_contents ( "php://input" );
-		if (! empty ( $str1 )) {
-			$postObj = simplexml_load_string ( $str1, 'SimpleXMLElement', LIBXML_NOCDATA );
-			if ($postObj !== false) {
-				$fromUser = $postObj->FromUserName;
-				$toUser = $postObj->ToUserName;
-				$tpl = "<xml>
+		if (isset ( $_GET ['echostr'] )) {
+			(new IndexController ())->valid ();
+		} else {
+			
+			$str2 = file_get_contents ( "php://input" );
+			if (! empty ( $str1 )) {
+				$postObj = simplexml_load_string ( $str1, 'SimpleXMLElement', LIBXML_NOCDATA );
+				if ($postObj !== false) {
+					$fromUser = $postObj->FromUserName;
+					$toUser = $postObj->ToUserName;
+					$tpl = "<xml>
                         <ToUserName><![CDATA[%s]]></ToUserName>
                         <FromUserName><![CDATA[%s]]></FromUserName>
                         <CreateTime>%s</CreateTime>
@@ -18,13 +22,15 @@ class TestController {
                         <Content><![CDATA[%s]]></Content>
                         <FuncFlag>0</FuncFlag>
                         </xml>";
-				if ($str1 == $str2) {
-					$resultStr = sprintf ( $tpl, $fromUser, $toUser, time (), "yeah" );
-				} else {
-					$resultStr = sprintf ( $tpl, $fromUser, $toUser, time (), "no" );
+					if ($str1 == $str2) {
+						$resultStr = sprintf ( $tpl, $fromUser, $toUser, time (), "yeah" );
+					} else {
+						$resultStr = sprintf ( $tpl, $fromUser, $toUser, time (), "no" );
+					}
+					exit ( $resultStr );
 				}
-				exit ( $resultStr );
 			}
+			exit ( "" );
 		}
 	}
 }
