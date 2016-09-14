a9264e0d13a8d2feee1c9c4ce5040b312eb1e523
linlite <cxiaolinj@163.com>
2016-09-14 11:13:51 +0800

    完全不明白为什么

--- Core/Controller/TestController.php
+++ Core/Controller/TestController.php
@@ -7,29 +7,30 @@ class TestController {
 		if (isset ( $_GET ['echostr'] )) {
 			(new IndexController ())->valid ();
 		} else {
-			
-			$str2 = file_get_contents ( "php://input" );
-			if (! empty ( $str2 )) {
-				$postObj = simplexml_load_string ( $str2, 'SimpleXMLElement', LIBXML_NOCDATA );
-				if ($postObj !== false) {
-					$fromUser = $postObj->FromUserName;
-					$toUser = $postObj->ToUserName;
-					$tpl = "<xml>
-                        <ToUserName><![CDATA[%s]]></ToUserName>
-                        <FromUserName><![CDATA[%s]]></FromUserName>
-                        <CreateTime>%s</CreateTime>
-                        <MsgType><![CDATA[text]]></MsgType>
-                        <Content><![CDATA[%s]]></Content>
-                        <FuncFlag>0</FuncFlag>
-                        </xml>";
-					if ($str1 == $str2) {
-						$resultStr = sprintf ( $tpl, $fromUser, $toUser, time (), "yeah" );
-					} else {
-						$resultStr = sprintf ( $tpl, $fromUser, $toUser, time (), "no" );
-					}
-					exit ( $resultStr );
-				}
-			}
+			(new IndexController())->response();
+// 			$str2 = file_get_contents ( "php://input" );
+// 			if (! empty ( $str2 )) {
+// 				$postObj = simplexml_load_string ( $str2, 'SimpleXMLElement', LIBXML_NOCDATA );
+// 				if ($postObj !== false) {
+// 					$fromUser = $postObj->FromUserName;
+// 					$toUser = $postObj->ToUserName;
+// 					$tpl = "<xml>
+//                         <ToUserName><![CDATA[%s]]></ToUserName>
+//                         <FromUserName><![CDATA[%s]]></FromUserName>
+//                         <CreateTime>%s</CreateTime>
+//                         <MsgType><![CDATA[text]]></MsgType>
+//                         <Content><![CDATA[%s]]></Content>
+//                         <FuncFlag>0</FuncFlag>
+//                         </xml>";
+// 					if ($str1 == $str2) {
+// 						$resultStr = sprintf ( $tpl, $fromUser, $toUser, time (), "yeah" );
+// 					} else {
+// 						$resultStr = sprintf ( $tpl, $fromUser, $toUser, time (), "no" );
+// 					}
+// 					exit ( $resultStr );
+// 				}
+// 			}
+
 			exit ( "" );
 		}
 	}
