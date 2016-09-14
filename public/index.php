a8b5bbc653e9dd16467d0edbba1b81d1d4a9e34a
linlite <cxiaolinj@163.com>
2016-09-14 10:48:36 +0800

    继续测试

--- public/index.php
+++ public/index.php
@@ -1,12 +1,16 @@
 <?php
-
-namespace linlite;
-
 use linlite\Core\Controller\IndexController;
+use linlite\Core\Controller\TestController;
+
+chdir ( dirname ( __DIR__ ) );
 
 include_once 'config/config.php';
 require 'autoloader.php';
 // ini_set("display_error", "on");
+// session_start();
+
+// $str1=file_get_contents("php://input");
+// TestController::run($str1);
 $c = new IndexController ();
 if (isset ( $_GET ['echostr'] )) {
 	$c->valid ();
